498c45e41a6db70f418c13bc00422c0021570eaf
MNabilZahinSiddiq <m.nabilzahinsiddiq@smkwikrama.sch.id>
2023-12-19 08:16:04 +0700

    tugas prod

---
+++ script.js
@@ -0,0 +1,44 @@
+// start
+let a = 18;
+let b = 8;
+
+let hasil = a + b
+
+console.log(`nomor 1: jadi penjumlahan dari ${a} dan ${b} adalah ${hasil}`)
+// end
+
+// start
+let jam = 1
+let detik = 3600
+
+let jumlah = jam * detik
+
+console.log(`nomor 2: ${jam} jam sama dengan ${jumlah} detik`)
+// end
+
+// start
+let jamM = 1
+let menit = 60
+
+let jumlahH = jamM * menit
+
+console.log(`nomor 3: ${jamM} jam sama dengan ${jumlahH} menit`)
+// end
+
+// start
+let KG = 1
+let G = 1000
+
+let berat = KG * G
+
+console.log(`nomor 4: ${KG} kilogram sama dengan ${berat} gram`)
+// end
+
+// start
+let KM = 1
+let M = 1000
+
+let jarak = KM * M
+
+console.log(`nomor 5: jadi jarak ${KM} kilometer adalah sama dengan ${jarak} meter`)
+// end
